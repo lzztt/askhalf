46c49ba851d67c65f0419f6d84c96de03e1b23ee
Long <15257546+lzztt@users.noreply.github.com>
2020-04-15 00:23:35 -0700

    update resume

--- yaml_to_html.py
+++ yaml_to_html.py
@@ -28,7 +28,7 @@ def create_html(yaml, repo_link):
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <title>Longzhang Tian's resume</title>
   <script>
     const yaml = `''' + yaml + r'''`
@@ -42,32 +42,34 @@ def create_html(yaml, repo_link):
         border-radius: 8px;
       }
     }
+    .container-fluid span {
+      white-space: pre-wrap;
+    }
   </style>
 </head>
 
 <body>
   <div class="container-fluid my-3" style="font-family: Courier New, Courier, monospace;">
     <div id="yaml"></div>
-    <footer class="text-secondary border-top mt-4 py-2" style="font-size: 0.9rem">
+    <footer class="text-black-50 border-top mt-4 py-2" style="font-size: 0.9rem">
       This site is open source.
       <a href="''' + repo_link + r'''">Improve this page</a>.
     </footer>
   </div>
 
   <script>
-    const re = /^( *)(- )?([\w ]*\w:)?(.*)$/
+    const re = /( *)(- )?([\w ]*\w: *)?(\[)?([^#\[\]]*)?(\])?(#.*)?/
     const html = yaml.split('\n').map(line => {
         groups = re.exec(line)
-        if (!groups[1]) {
-            groups[1] = ''
-        } else {
-            groups[1] = '&nbsp;'.repeat(groups[1].length);
-        }
+        groups[1] = groups[1] ? `<span>${groups[1]}</span>` : ''
         groups[2] = groups[2] ? `<span class="text-primary">${groups[2]}</span>` : ''
         groups[3] = groups[3] ? `<span class="text-success">${groups[3]}</span>` : ''
-        groups[4] = groups[4] ? `<span class="text-dark">${groups[4]}</span>` : ''
-        return groups[1] + groups[2] + groups[3] + groups[4] + '<br>'
-    }).join('')
+        groups[4] = groups[4] ? `<span class="text-black-50">${groups[4]}</span>` : ''
+        groups[5] = groups[5] ? `<span class="text-dark">${groups[5]}</span>` : ''
+        groups[6] = groups[6] ? `<span class="text-black-50">${groups[6]}</span>` : ''
+        groups[7] = groups[7] ? `<span class="text-black-50">${groups[7]}</span>` : ''
+        return groups.slice(1, 8).join('')
+    }).join('<br>')
 
     document.getElementById('yaml').innerHTML = html
   </script>
